91cf2dfdc3a4e89bf15b20706088d436b995a220
aminul <aminulamin0001@gmail.com>
2026-06-02 22:57:12 +0600

    cover up the compariosn operations

---
+++ 13_JavaScript/01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// comparsion operators
+// ==, ===, !=, !==, >, <, >=, <=
+
+console.log(5 == "5"); // true (loose equality)
+console.log(5 === "5"); // false (strict equality)
+console.log(5 != "5"); // false (loose inequality)
+console.log(5 !== "5"); // true (strict inequality)
+console.log(5 > 3); // true
+console.log(5 < 3); // false
+console.log(5 >= 5); // true
+console.log(5 <= 4); // false
+console.log(5 > 5); // false
+console.log(5 < 5); // false
+console.log(null > 0); // false 
+console.log(null >= 0); // ture
